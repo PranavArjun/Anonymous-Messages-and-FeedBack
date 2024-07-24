3e12fc29c74dde3d1d93169b858be19e3ab8af99
Pranav <pranavarjun14@gmail.com>
2024-07-24 17:10:02 +0530

    Create Sign-Up page frontend using shadcn , react-hook form

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod" ;
 
 export const usernameValidation = z
 .string()
-.min(2,"Username must be atleast 2 characters")
+.min(5,"Username must be atleast 5 characters")
 .max(20,"Username must not be greater than 20 characters")
 .regex(/^[a-zA-Z0-9_]+$/, 'Username must not contain special characters');
 
